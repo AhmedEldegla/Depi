2ab992120df1c27474a95df59dcb8d1d9979c1f5
McDiesel <ahmeddagla99@gmail.com>
2026-04-10 05:07:53 +0200

    Test Pull Request

--- Depi.Domain/Modules/Identity/Entities/Role.cs
+++ Depi.Domain/Modules/Identity/Entities/Role.cs
@@ -8,6 +8,6 @@ public class Role : BaseEntity
     public string Code { get; set; } = string.Empty;
     public string? Description { get; set; }
 
-    public ICollection<UserRole> UserRoles { get; set; } = new List<UserRole>();
-    public ICollection<RolePermission> RolePermissions { get; set; } = new List<RolePermission>();
+    public ICollection<UserRole> UserRoles { get; set; } = new HashSet<UserRole>();
+    public ICollection<RolePermission> RolePermissions { get; set; } = new HashSet<RolePermission>();
 }
