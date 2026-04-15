7c7cb1ea1f3e28c9f2f12477a975e9cf2c842780
Hamsa Alaa <hamsa2002alaa@gmail.com>
2026-04-16 01:52:35 +0200

    add coaching session entities

---
+++ Depi.Domain/Modules/Profiles/Entities/UserLink.cs
@@ -0,0 +1,17 @@
+﻿using DEPI.Domain.Common.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Depi.Domain.Modules.Profiles.Entities
+{
+    public class UserLink : BaseEntity
+    {
+        public Guid UserId { get; set; }
+        public LinkType Type { get; set; }
+        public string Url { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
---
+++ Depi.Domain/Modules/Profiles/Entities/UserProfile.cs
@@ -0,0 +1,26 @@
+﻿using DEPI.Domain.Common.Base;
+using DEPI.Domain.Modules.Identity.Entities;
+using DEPI.Domain.Modules.Country.Entities;
+
+
+namespace DEPI.Domain.Modules.Profiles.Entities
+{
+    public class UserProfile : BaseEntity
+    {
+        public Guid UserId { get; set; }
+
+        public string? Bio { get; set; }
+        public string? Headline { get; set; }
+        public string? Summary { get; set; }
+        public decimal? HourlyRate { get; set; }
+
+        public Guid? CountryId { get; set; }
+        public string? City { get; set; }
+        public string? Address { get; set; }
+        public string? Timezone { get; set; }
+        public int? Availability { get; set; }
+
+        public User User { get; set; } = null!;
+        public Country? Country { get; set; }
+    }
+}
