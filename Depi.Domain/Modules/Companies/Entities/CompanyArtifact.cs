a3d7bd1e2ae4f389105e6ae89046be6c2d67c83f
fatmahassann <fatfatthassan2@gmail.com>
2026-04-15 10:58:36 +0200

    feat(Companies) : ChangeFolderName

--- Depi.Domain/Modules/Companies/Entities/CompanyArtifact.cs
+++ Depi.Domain/Modules/Companies/Entities/CompanyArtifact.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using DEPI.Domain.Common.Base;
+using DEPI.Domain.Modules.Companies.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Depi.Domain.Modules.Company_Profiles.Entities
+namespace DEPI.Domain.Modules.Companies.Entities
 {
-    internal class CompanyMembers
+    public class CompanyArtifact :AuditableEntity
     {
+        public string ArtifactType { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public string Url { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        // Foreign Keys and Navigation Properties
+        public Guid CompanyId { get; set; }
+        public Company? Company { get; set; }
+        public Guid? MediaId { get; set; }
+        //public MediaFiles? Media { get; set; }
     }
+
+
+
 }
