f83597673716685107286e1e25ef9f6fdb68a347
fatmahassann <fatfatthassan2@gmail.com>
2026-04-16 14:20:28 +0200

    Feat(ProjectProposalsDelivery): Initialize Project,Proposals,Delivery Module

---
+++ Depi.Domain/Modules/ProjectProposalsDelivery/Entities/Proposal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DEPI.Domain.Common.Base;
+using Depi.Domain.Modules.ProjectProposalsDelivery.Enums;
+
+
+
+namespace Depi.Domain.Modules.ProjectProposalsDelivery.Entities
+{
+    public class Proposal : AuditableEntity
+    {
+        public string CoverLetter { get; set; } = string.Empty;
+        public decimal ProposedAmount { get; set; }
+        public int? DeliveryDays { get; set; }
+        public ProposalStatus Status { get; set; }
+
+        // Forign keys & Navigation Properties
+        public Guid ProjectId { get; set; }
+        public Project Project { get; set; }
+        public Guid FreelancerId { get; set; }
+        //public Freelancer Freelancer { get; set; }
+        public Contract? Contract { get; set; }
+
+
+    }
+}
