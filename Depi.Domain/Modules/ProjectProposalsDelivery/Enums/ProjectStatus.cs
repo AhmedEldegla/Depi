761e2047604b83e9380a6a7019b53a5cb12e2f1a
fatmahassann <fatfatthassan2@gmail.com>
2026-04-16 14:20:28 +0200

    Feat(ProjectProposalsDelivery): Initialize Project,Proposals,Delivery Module

---
+++ Depi.Domain/Modules/ProjectProposalsDelivery/Enums/ProjectStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Depi.Domain.Modules.ProjectProposalsDelivery.Enums
+{
+    public enum ProjectStatus
+    {
+        Draft = 1,
+        Open = 2,
+        Closed = 3,
+        InProgress = 4 ,
+        Completed = 5
+    }
+}
---
+++ Depi.Domain/Modules/ProjectProposalsDelivery/Enums/ProposalStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Depi.Domain.Modules.ProjectProposalsDelivery.Enums
+{
+    public enum ProposalStatus 
+    {
+        Submitted = 1,
+        Withdrawn = 2,
+        Shortlisted = 3,
+        Rejected = 4,
+        Hired = 5
+
+    }
+}
