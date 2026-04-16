d049fd3075c83873e5795c02b9b192184875c209
Hamsa Alaa <74626772+Hamsa22alaa@users.noreply.github.com>
2026-04-16 02:57:31 +0200

    Implement Profiles , Payments and Coaching  modules (Domain Layer) (#5)
    * add coaching session entities
    
    * Profiles module - domain implementation
    
    * Payments module - domain implementation
    
    * Coaching module - domain implementation

---
+++ Depi.Domain/Modules/Coaching/Entities/CoachingSession.cs
@@ -0,0 +1,29 @@
+﻿using DEPI.Domain.Common.Base;
+using DEPI.Domain.Modules.Identity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Depi.Domain.Modules.Coaching.Entities
+{
+    public class CoachingSession : BaseEntity
+    {
+        public Guid CoachId { get; set; }
+        public Guid StudentId { get; set; }
+        public string Topic { get; set; }
+        public string? Description { get; set; }
+        public SessionType Type { get; set; }
+        public SessionStatus Status { get; set; }
+        public DateTime ScheduledAt { get; set; }
+        public int DurationInMinutes { get; set; }
+        public decimal Price { get; set; }
+        public decimal? Rating { get; set; }
+        public string? Feedback { get; set; }
+        public User Coach { get; set; } = null!;
+        public User Student { get; set; } = null!;
+
+
+    }
+}
---
+++ Depi.Domain/Modules/Profiles/Entities/UserSetting.cs
@@ -0,0 +1,17 @@
+﻿using DEPI.Domain.Common.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Depi.Domain.Modules.Profiles.Entities
+{
+    public class UserSetting : BaseEntity
+    {
+        public Guid UserId { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+
+    }
+}
