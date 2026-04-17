637a15b44eea9f4a73041af5113d5365e83a8307
Ahmed Eldegla <32678755+AhmedEldegla@users.noreply.github.com>
2026-04-17 07:12:57 +0200

    Add Module 06 - Guild Domain (Entities & Enums) (#9)
    Co-authored-by: Mahmoud heggy <hem2742@gmail.com>

---
+++ Depi.Domain/Modules/Guild/Enities/GuildSkill.cs
@@ -0,0 +1,13 @@
+﻿using DEPI.Domain.Common.Base;
+
+namespace Depi.Domain.Modules.Guild.Entities;
+
+public class GuildSkill : BaseEntity
+{
+    public Guid GuildId { get; set; }
+
+    public Guid SkillId { get; set; }
+
+    // Navigation Properties
+    public virtual Guild Guild { get; set; } = null!;
+}
