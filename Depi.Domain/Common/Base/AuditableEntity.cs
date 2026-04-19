3dda63e36f55820f38e7ef9839a2fb6844f1f8a5
McDiesel <ahmeddagla99@gmail.com>
2026-04-19 21:48:54 +0200

    refactor(domain): reorganize base domain

---
+++ Depi.Domain/Common/Base/AuditableEntity.cs
@@ -0,0 +1,26 @@
+﻿
+namespace DEPI.Domain.Common.Base;
+
+public abstract class AuditableEntity : BaseEntity
+{
+    public Guid CreatedBy { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public Guid? UpdatedBy { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public Guid? DeletedBy { get; set; }
+    public DateTime? DeletedAt { get; set; }
+    public bool IsDeleted { get; protected set; }
+
+    public void MarkAsDeleted(Guid deletedBy)
+    {
+        IsDeleted = true;
+        DeletedBy = deletedBy;
+        DeletedAt = DateTime.UtcNow;
+    }
+
+    public void MarkAsUpdated(Guid updatedBy)
+    {
+        UpdatedBy = updatedBy;
+        UpdatedAt = DateTime.UtcNow;
+    }
+}
