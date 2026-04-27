d97acd14ccd07fd5abb2ecdd141ca8540d7fafda
fatmahassann <fatfatthassan2@gmail.com>
2026-04-27 18:36:33 +0300

    Feat(Wallets): add Wallet, Transaction and Escrow commands

---
+++ Depi.Application/UseCases/Wallets/Commands/FundEscrowCommand.cs
@@ -0,0 +1,14 @@
+﻿using Depi.Application.DTOs.Wallets;
+using DEPI.Application.Common;
+using MediatR;
+
+namespace DEPI.Application.UseCases.Wallets.Commands
+{
+    public record FundEscrowCommand(
+        Guid UserId,
+        Guid EscrowId
+    ) : IRequest<Result<EscrowResponseDto>>;
+
+}
+
+
---
+++ Depi.Application/UseCases/Wallets/Commands/TransferCommand.cs
@@ -0,0 +1,16 @@
+﻿using Depi.Application.DTOs.Wallets;
+using DEPI.Application.Common;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Depi.Application.UseCases.Wallets.Commands
+{
+    public record TransferCommand(
+        Guid UserId,
+        TransferRequestDto Request
+    ) : IRequest<Result<TransactionResponseDto>>;
+}
