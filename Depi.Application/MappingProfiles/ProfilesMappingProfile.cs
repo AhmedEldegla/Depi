d2c75c24111edf46cc1cf9eec0825c540ffa1db3
McDiesel <ahmeddagla99@gmail.com>
2026-05-02 18:57:52 +0300

    feat(Application) : Add ProfilesMappingProfile for entity-to-DTO mappings
    Introduces the `ProfilesMappingProfile` class to define AutoMapper mappings between domain entities and their corresponding DTOs. This includes mappings for `UserProfile`, `ServicePackage`, `PortfolioItem`, `Skill`, and `FreelancerSkill`, with custom configurations for properties like `CurrencyCode` and `CountryName` to handle null values appropriately.

---
+++ Depi.Application/MappingProfiles/ProfilesMappingProfile.cs
@@ -0,0 +1,24 @@
+using AutoMapper;
+using DEPI.Application.DTOs.Profiles;
+using DEPI.Domain.Entities.Profiles;
+
+namespace DEPI.Application.MappingProfiles;
+
+public class ProfilesMappingProfile : Profile
+{
+    public ProfilesMappingProfile()
+    {
+        CreateMap<UserProfile, UserProfileResponse>()
+            .ForMember(dest => dest.CurrencyCode, opt => opt.MapFrom(src => src.Currency != null ? src.Currency.Code : null))
+            .ForMember(dest => dest.CountryName, opt => opt.MapFrom(src => src.Country != null ? src.Country.Name : null));
+
+        CreateMap<ServicePackage, ServicePackageResponse>()
+            .ForMember(dest => dest.CurrencyCode, opt => opt.MapFrom(src => src.Currency != null ? src.Currency.Code : null));
+
+        CreateMap<PortfolioItem, PortfolioItemResponse>();
+        CreateMap<Skill, SkillResponse>();
+
+        CreateMap<FreelancerSkill, FreelancerSkillResponse>()
+            .ForMember(dest => dest.SkillName, opt => opt.MapFrom(src => src.Skill != null ? src.Skill.Name : null));
+    }
+}
