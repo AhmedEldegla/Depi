8a3dcecb76e648dc480b7191e51a5a2c477ad395
Hamsa Alaa <hamsa2002alaa@gmail.com>
2026-05-07 20:14:22 +0300

    update ProjectsController

--- Depi.API/Controllers/ProjectsController.cs
+++ Depi.API/Controllers/ProjectsController.cs
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-﻿using DEPI.Application.UseCases.Projects.CreateProject;
-using DEPI.Application.UseCases.Projects.UpdateProject;
-using DEPI.Application.UseCases.Projects.Queries;
-using MediatR;
-=======
-using DEPI.Application.DTOs.Projects;
+﻿using DEPI.Application.DTOs.Projects;
 using DEPI.Application.UseCases.Projects.CreateProject;
 using DEPI.Application.UseCases.Projects.DeleteProject;
 using DEPI.Application.UseCases.Projects.OpenProject;
@@ -12,7 +6,6 @@ using DEPI.Application.UseCases.Projects.UpdateProject;
 using DEPI.Application.UseCases.Projects.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
->>>>>>> master
 using Microsoft.AspNetCore.Mvc;
 
 namespace DEPI.API.Controllers;
@@ -22,61 +15,6 @@ namespace DEPI.API.Controllers;
 public class ProjectsController : ControllerBase
 {
     private readonly IMediator _mediator;
-<<<<<<< HEAD
-
-    public ProjectsController(IMediator mediator)
-    {
-        _mediator = mediator;
-    }
-
-    // Create
-    [HttpPost]
-    public async Task<IActionResult> Create(CreateProjectCommand command)
-    {
-        var result = await _mediator.Send(command);
-
-        if (!result.IsSuccess)
-            return BadRequest(result);
-
-        return Ok(result);
-    }
-
-    // Get by Id
-    [HttpGet("{id}")]
-    public async Task<IActionResult> GetById(Guid id)
-    {
-        var result = await _mediator.Send(new GetProjectQuery(id));
-
-        if (!result.IsSuccess)
-            return NotFound(result);
-
-        return Ok(result);
-    }
-
-    // Get All (with filters)
-    [HttpGet]
-    public async Task<IActionResult> GetAll([FromQuery] GetProjectsQuery query)
-    {
-        var result = await _mediator.Send(query);
-        return Ok(result);
-    }
-
-    // Update
-    [HttpPut("{id}")]
-    public async Task<IActionResult> Update(Guid id, UpdateProjectCommand command)
-    {
-        if (id != command.ProjectId)
-            return BadRequest("Id mismatch");
-
-        var result = await _mediator.Send(command);
-
-        if (!result.IsSuccess)
-            return BadRequest(result);
-
-        return Ok(result);
-    }
-}
-=======
     public ProjectsController(IMediator mediator) => _mediator = mediator;
 
     [HttpGet]
@@ -150,4 +88,3 @@ public class ProjectsController : ControllerBase
         return Guid.TryParse(sub, out var uid) ? uid : Guid.Empty;
     }
 }
->>>>>>> master
